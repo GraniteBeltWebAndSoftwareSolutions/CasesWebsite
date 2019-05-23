dd3199896e56ada15259960ea01bd6afebef1982
GraniteBeltWeb <40533161+GraniteBeltWebAndSoftwareSolutions@users.noreply.github.com>
2019-05-23 22:51:16 +1000

    Add files via upload

--- Case's site - CODE/sqldatabase.php
+++ Case's site - CODE/sqldatabase.php
@@ -1,110 +1,96 @@
-<?php 
+<?php
+  $host = 'localhost';
+  $user = 'akkyo7dw6lhh';
+  $password = '0101RdRZ#';
+  $dbname = 'gaiman';
+  // Set DSN
+  $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+  // Create a PDO instance
+  $connection = new PDO($dsn, $user, $password);
+  // Set PDO::FETCH_OBJ as fetch() default attributes
+  $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+  // Enables or disables emulation of prepared statements (and allows us to set placeholders for our limits)
+  $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  // PDO QUERY
+  // Go grab all posts
+  // $stmt = $connection->query('SELECT * FROM posts');
+  // Display all titles
+  // PDO::FETCH_ASSOC: returns an array indexed by column name as returned in your result set
+  // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  //   echo $row['title'] . '<br>';
+  // }
+  // while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+  //   echo $row->title . '<br>';
+  // }
+  // PREPARED STATEMENTS (prepare & execute)
+  // UNSAFE
+  // $sql = "SELECT * FROM posts WHERE author = '$author'";
+  // FETCH MULTIPLE POSTS
+  // User Input
+
+  echo "<a href='snakeLikeGameJavascript.html'>SnakeLikeGame</a>";
 
 
-echo "click arrow to go back now ";
-echo "<a href='snakeLikeGameJavascript.html'>Click To Play Snake</a>";
+  $author = 'Brad';
+  $isPublished = true;
+  $id = 1;
+  $limit = 1;
+  // Positional Params
+  //$sql = 'SELECT * FROM posts WHERE author = ? && is_published = ? LIMIT ?';
+  $sql = 'SELECT * FROM books';
+  $stmt = $connection->prepare($sql);
+  $stmt->execute([$author, $isPublished, $limit]);
+  $posts = $stmt->fetchAll();
+  // Named Params
+  // $sql = 'SELECT * FROM posts WHERE author = :author && is_published = :is_published';
+  // $stmt = $connection->prepare($sql);
+  // $stmt->execute(['author' => $author, 'is_published' => $isPublished]);
+  // $posts = $stmt->fetchAll();
+  // var_dump($posts);
+  foreach ($posts as $post) {
+    echo $post->title . '<br>';
+  }
 
 
-?>
-
- <?php
-
-
-	
-	
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'akkyo7dw6lhh');
-	define('DB_PASS', '0101RdRZ#');
-	define('DB_NAME', 'gaiman');
-		// Create Connection
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-	// Check Connection
-	if(mysqli_connect_errno()){
-		// Connection Failed
-		echo 'Failed to connect to MySQL '. mysqli_connect_errno();
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	echo "This has connected";
-	
-
-	// Create Query
-	$query = 'SELECT * FROM books WHERE id=1';   // changed this here
-echo "This has connected2";
-	// Get Result
-	$result = mysqli_query($conn, $query);
-echo "This has connected3";
-	// Fetch Data
-	$posts = mysqli_fetch_all($result, MYSQLI_ASSOC); //it don't like this line
-	var_dump($posts);                       // added this here
-	//var_dump($posts);
-echo "This has connected4";
-	// Free Result
-	mysqli_free_result($result);
-echo "This has connected5";
-	// Close Connection
-	mysqli_close($conn);
-echo "This has connected6";
-	
-	
-	
-	
-	echo "This has connected";
-	
-	echo "This has connected";
-?>
-
-<?php  ?>
-	<div class="container">
-		<h1>Posts</h1>
-		<?php foreach($posts as $post) : ?>
-			<div class="well">
-				<h1><?php echo $post['author'];
-				echo strlen($post['author']);
-
-
-
-				?></h1>
-				<h3><?php echo $post['title']; ?></h3>
-				<small>A book titled <?php echo $post['title']; ?> by <?php echo $post['author']; ?>
-				
-				with a strlen() of <?php echo strlen($post['title']); ?>
-				
-				for the title <?php 
-				
-				
-				?>
-				
-				
-				
-				</small>
-				<p><?php echo $post['cost']; ?></p>
-				<a class="btn btn-default" href="<?php echo ROOT_URL; ?>post.php?id=<?php echo $post['id']; ?>">Read More</a>
-			</div>
-		<?php endforeach; ?>
-	</div>
-<?php include('inc/footer.php');
-
-
-
-
- ?>
-
-
-
-
-
-
-
-
-
-
+  echo "Hi there";
+  // FETCH SINGLE POST
+  // $sql = 'SELECT * FROM posts WHERE id = :id';
+  // $stmt = $connection->prepare($sql);
+  // $stmt->execute(['id' => $id]);
+  // $post = $stmt->fetch();
+  // echo $post->body;
+  // GET ROW COUNT
+  // $stmt = $connection->prepare('SELECT * FROM POSTS WHERE author = ?');
+  // $stmt->execute([$author]);
+  // $postCount = $stmt->rowCount();
+  // echo $postCount;
+  // INSERT DATA
+    // $title = 'Post Five';
+    // $body = 'This is post five';
+    // $author = 'Kevin';
+    // $sql = 'INSERT INTO posts(title, body, author) VALUES(:title, :body, :author)';
+    // $stmt = $connection->prepare($sql);
+    // $stmt->execute(['title' => $title, 'body' => $body, 'author' => $author]);
+    // echo 'Post Added';
+    // UPDATE DATA
+    // $id = 1;
+    // $body = 'This is the updated post';
+    // $sql = 'UPDATE posts SET body = :body WHERE id = :id';
+    // $stmt = $connection->prepare($sql);
+    // $stmt->execute(['body' => $body, 'id' => $id]);
+    // echo 'Post Updated';
+    // DELETE DATA
+    // $id = 3;
+    // $sql = 'DELETE FROM posts WHERE id = :id';
+    // $stmt = $connection->prepare($sql);
+    // $stmt->execute(['id' => $id]);
+    // echo 'Post Deleted';
+    // SEARCH DATA
+    // $search = "%f%";
+    // $sql = 'SELECT * FROM posts WHERE title LIKE ?';
+    // $stmt = $connection->prepare($sql);
+    // $stmt->execute([$search]);
+    // $posts = $stmt->fetchAll();
+    // foreach ($posts as $post) {
+    //   echo $post->title . '<br>';
+    // }
